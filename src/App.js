2f8f05cb5b8a2f797c531882a89757b63c8ab4c2
Munir Mahmud <munirmahmud6@gmail.com>
2020-11-15 21:15:28 +0600

    Banner component done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Banner from './components/Banner/Banner';
 import Navigation from './components/Navigation/Navigation';
 
 const App = () => {
     return (
         <Router>
             <Navigation />
+            <Banner />
         </Router>
     );
 };
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Banner.styles.css';
+
+const Banner = () => {
+    return (
+        <div className="container">
+            <header>
+                 <div className="headerText">
+                     <div>
+                        <h1>Happy family, happy smile</h1>
+                        <p>Enjoy online shopping with us</p>
+                     </div>
+                 </div>
+            </header>
+        </div>
+            
+    );
+};
+
+export default Banner;
